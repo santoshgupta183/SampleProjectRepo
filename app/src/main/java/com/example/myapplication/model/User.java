a226052c083e5ab58021227b5de93f053ade7709
santoshgupta183 <59694579+santoshgupta183@users.noreply.github.com>
2020-01-20 18:24:36 +0530

    address added to user

--- app/src/main/java/com/example/myapplication/model/User.java
+++ app/src/main/java/com/example/myapplication/model/User.java
@@ -5,15 +5,17 @@ public class User {
     private String fName;
     private String lName;
     private int age;
+    private String address;
 
     public User() {
     }
 
-    public User(String id, String fName, String lName, int age) {
+    public User(String id, String fName, String lName, int age, String address) {
         this.id = id;
         this.fName = fName;
         this.lName = lName;
         this.age = age;
+        this.address = address;
     }
 
     public String getId() {
@@ -46,5 +48,13 @@ public class User {
 
     public void setAge(int age) {
         this.age = age;
+    }
+    
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 }
