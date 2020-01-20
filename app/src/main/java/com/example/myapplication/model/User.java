b6742821280f40839c3ac2ef318433539c302456
santosh14143 <santosh.gupta183@gmail.com>
2020-01-20 18:18:28 +0530

    Created User POJO Class

---
+++ app/src/main/java/com/example/myapplication/model/User.java
@@ -0,0 +1,47 @@
+package com.example.myapplication.model;
+
+public class User {
+    private String id;
+    private String fName;
+    private String lName;
+    private int age;
+
+    public User(String id, String fName, String lName, int age) {
+        this.id = id;
+        this.fName = fName;
+        this.lName = lName;
+        this.age = age;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
